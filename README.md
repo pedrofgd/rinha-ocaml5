1f277c0d388ce69f6456acfcd13716bcc476715d
Pedro Dias <mnopedrodias@gmail.com>
2023-10-04 21:34:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+`dune build` and 
+`dune exec rinha_ocaml5` (bind at 9999)
---
+++ bin/main.ml
@@ -0,0 +1,66 @@
+type person = {
+  id : string option;
+  nome : string option;
+  apelido : string option;
+  nascimento : string option;
+  stack : string list option;
+}
+[@@deriving yojson]
+
+let generate_uuid =
+  Uuidm.to_string ~upper:true (Uuidm.v (`V3 (Uuidm.ns_dns, "www.example.org")))
+
+let create_handler _request =
+  Dream.log "create_handler initialized";
+  let uuid = generate_uuid in
+  let location = String.cat "/pessoas/" uuid in
+  let response =
+    Dream.response ~status:`Created "created"
+      ~headers:[ ("Location", location) ]
+  in
+  Lwt.return response
+
+let get_by_id _request =
+  Dream.log "get_by_id initialized";
+  let id = Dream.param _request "id" in
+  let p =
+    {
+      id = Some id;
+      nome = None;
+      apelido = None;
+      nascimento = None;
+      stack = None;
+    }
+  in
+  let json_response = Yojson.Safe.to_string (person_to_yojson p) in
+  Dream.json json_response
+
+let get_by_term _request =
+  Dream.log "get_by_term initialized";
+  let term = Dream.query _request "t" in
+  match term with
+  | Some search_term ->
+      let person =
+        {
+          id = Some generate_uuid;
+          nome = None;
+          apelido = Some search_term;
+          nascimento = None;
+          stack = None;
+        }
+      in
+      let json_response = `List [ person_to_yojson person ] in
+      Dream.json (Yojson.Safe.to_string json_response)
+  | None -> Dream.empty `Bad_Request
+
+let count _request = Dream.respond "5"
+
+let () =
+  Dream.run ~port:9999 @@ Dream.logger
+  @@ Dream.router
+       [
+         Dream.post "/pessoas" create_handler;
+         Dream.get "/pessoas/:id" get_by_id;
+         Dream.get "/pessoas" get_by_term;
+         Dream.get "/contagem-pessoas" count;
+       ]
